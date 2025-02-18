a5416e9f330e63b847fda635652b78ea3cff9500
Andrew Kombe <andrewnzaikombe@gmail.com>
2025-02-18 16:22:31 +0300

    Renamed BitcoinWalletService to BitcoinService

--- backend/core/src/main/java/com/kollybistes/core/services/BitcoinService.java
+++ backend/core/src/main/java/com/kollybistes/core/services/BitcoinService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class BitcoinWalletService {
+public class BitcoinService {
 
     private final BitcoinWalletRepository bitcoinWalletRepository;
     private final AuthService authService;
