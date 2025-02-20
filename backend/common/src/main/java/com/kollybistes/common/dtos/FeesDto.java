5f7405d585ccd995e5e2c218d1f66d3294dc6a4c
Andrew Kombe <andrewnzaikombe@gmail.com>
2025-02-20 10:00:19 +0300

    Added new Dtos

---
+++ backend/common/src/main/java/com/kollybistes/common/dtos/FeesDto.java
@@ -0,0 +1,17 @@
+package com.kollybistes.common.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FeesDto {
+    private BigDecimal systemFee;
+    private BigDecimal transactionFee;
+}
