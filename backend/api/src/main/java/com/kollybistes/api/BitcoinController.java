a91b6059dfd9f77f3884c8a6af7a23a89dd45141
Andrew Kombe <andrewnzaikombe@gmail.com>
2025-02-20 10:22:26 +0300

    Added API paths

--- backend/api/src/main/java/com/kollybistes/api/BitcoinController.java
+++ backend/api/src/main/java/com/kollybistes/api/BitcoinController.java
@@ -1,12 +1,10 @@
 package com.kollybistes.api;
 
 import com.kollybistes.common.dtos.APIResponse;
+import com.kollybistes.common.dtos.TransactionDto;
 import com.kollybistes.core.services.BitcoinService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/bitcoin/")
@@ -32,6 +30,31 @@ public class BitcoinController {
         try {
             return bitcoinService.getWalletBalance();
         } catch (Exception e) {
+            return APIResponse
+                    .builder()
+                    .error(e.getMessage())
+                    .build();
+        }
+    }
+
+    @PostMapping("send")
+    public Object sendBitcoinToOutsideWallet(@RequestBody TransactionDto transactionDto){
+        try{
+            return bitcoinService.sendBitcoinToOutsideWallet(transactionDto.getRecipientAddress(),
+                    transactionDto.getAmount());
+        }catch(Exception e){
+            return APIResponse
+                    .builder()
+                    .error(e.getMessage())
+                    .build();
+        }
+    }
+
+    @PostMapping("confirmTransaction")
+    public Object confirmTransaction(@RequestBody TransactionDto transactionDto){
+        try{
+            return bitcoinService.confirmTransaction(transactionDto);
+        }catch(Exception e){
             return APIResponse
                     .builder()
                     .error(e.getMessage())
