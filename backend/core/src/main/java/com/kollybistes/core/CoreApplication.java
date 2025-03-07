00563d445310d53dba0a081bf2b337635847d62d
Andrew Kombe <andrewnzaikombe@gmail.com>
2025-03-07 12:35:26 +0300

    Removed unnecessary package scan

--- backend/core/src/main/java/com/kollybistes/core/CoreApplication.java
+++ backend/core/src/main/java/com/kollybistes/core/CoreApplication.java
@@ -3,12 +3,11 @@ package com.kollybistes.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 //@ComponentScan(basePackages = {"com.kollybistes.common"})
-@EntityScan(basePackages = {"com.kollybistes.common.models", "com.kollybistes.core.models"})
+@EntityScan(basePackages = {"com.kollybistes.common.models"})
 @EnableJpaRepositories(basePackages = "com.kollybistes.core.repositories")
 public class CoreApplication {
     public static void main(String[] args) {
