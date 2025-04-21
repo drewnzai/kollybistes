5c39fe58d6a41d99623bbf87f939902c2109475c
Andrew Kombe <andrewnzaikombe@gmail.com>
2025-04-21 15:55:31 +0300

    Added id field

--- backend/common/src/main/java/com/kollybistes/common/models/Transaction.java
+++ backend/common/src/main/java/com/kollybistes/common/models/Transaction.java
@@ -4,8 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
@@ -14,6 +13,9 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String senderWalletAddress;
     private String recipientWalletAddress;
     private BigDecimal amount;
