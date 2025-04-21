7efa1542f20fa4b28b51614a7e58c368fca4b6c8
Andrew Kombe <andrewnzaikombe@gmail.com>
2025-04-21 16:05:43 +0300

    Added Builder annotation and created TransactionService

--- backend/common/src/main/java/com/kollybistes/common/models/Transaction.java
+++ backend/common/src/main/java/com/kollybistes/common/models/Transaction.java
@@ -1,6 +1,7 @@
 package com.kollybistes.common.models;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
@@ -12,6 +13,7 @@ import java.util.Date;
 @Entity
 @Table(name = "transactions")
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Transaction {
---
+++ backend/core/src/main/java/com/kollybistes/core/services/TransactionService.java
@@ -0,0 +1,33 @@
+package com.kollybistes.core.services;
+
+import com.kollybistes.common.models.Transaction;
+import com.kollybistes.core.repositories.TransactionRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.Date;
+
+@Service
+@AllArgsConstructor
+public class TransactionService {
+    private final TransactionRepository transactionRepository;
+
+    @Async
+    public void saveTransaction(String sender,
+                                String recipient,
+                                BigDecimal amount,
+                                String txHash){
+        Transaction transaction = Transaction.builder()
+                .senderWalletAddress(sender)
+                .recipientWalletAddress(recipient)
+                .amount(amount)
+                .transactionHash(txHash)
+                .createdAt(Date.from(Instant.now()))
+                .build();
+
+        transactionRepository.save(transaction);
+    }
+}
