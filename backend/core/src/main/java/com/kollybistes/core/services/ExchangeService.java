9f879f43d5148f44249f00dcae03a25b0ea6c1b8
Andrew Kombe <andrewnzaikombe@gmail.com>
2025-04-21 18:50:01 +0300

    Going on tangent to create BitcoinRPC methods

--- backend/core/src/main/java/com/kollybistes/core/services/ExchangeService.java
+++ backend/core/src/main/java/com/kollybistes/core/services/ExchangeService.java
@@ -1,10 +1,67 @@
 package com.kollybistes.core.services;
 
+import com.kollybistes.common.dtos.ExchangeDto;
+import com.kollybistes.common.models.BitcoinWallet;
+import com.kollybistes.common.models.User;
+import com.kollybistes.core.repositories.BitcoinWalletRepository;
+import com.kollybistes.core.repositories.EthereumRepository;
+import com.kollybistes.core.repositories.ExchangeRepository;
+import com.kollybistes.core.rpc.BitcoinRPC;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.math.BigInteger;
 
 @Service
 @RequiredArgsConstructor
 public class ExchangeService {
+    private final ExchangeRepository exchangeRepository;
+    private final APIHandler apiHandler;
+    private final AuthService authService;
+    private final EthereumRepository ethereumRepository;
+    private final BitcoinWalletRepository bitcoinWalletRepository;
+    private final BitcoinRPC bitcoinRPC;
+
+    @Value("${system.btc.address}")
+    private String systemBtcAddress;
+
+    @Value("${system.eth.address}")
+    private String systemEthAddress;
+
+    @Value("${system.eth.chainId}")
+    private String ethChainId;
+
+    public ExchangeDto calculateExchangeDetails(ExchangeDto exchangeDto){
+        if(exchangeDto.getTradeType().equalsIgnoreCase("BTC_TO_ETH")){
+            return calculateBtcToEth(exchangeDto);
+        }
+        else{
+            return calculateEthToBtc(exchangeDto);
+        }
+        return null;
+    }
+
+    private ExchangeDto calculateBtcToEth(ExchangeDto exchangeDto) throws Exception {
+        User user = authService.getCurrentUser();
+
+        BitcoinWallet bitcoinWallet = bitcoinWalletRepository.findByUser(user)
+                .orElseThrow(() -> new Exception("User does not have a Bitcoin wallet"));
+
+
+    }
+
+    private BigInteger updateBalance(BitcoinWallet bitcoinWallet){
+        BigInteger updated = bitcoinRPC
+                .getTrustedAddressBalance(bitcoinWallet
+                        .getUser()
+                        .getUsername());
+
+        bitcoinWallet.setBalance(updated);
+
+        bitcoinWalletRepository.save(bitcoinWallet);
+
+        return updated;
+    }
 
 }
