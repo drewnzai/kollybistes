6ee10d4ebe62a768ba1a8be6e591cda134e70391
Andrew Kombe <andrewnzaikombe@gmail.com>
2025-04-21 17:52:54 +0300

    Renamed Trade entity and related models to Exchange

--- backend/core/src/main/java/com/kollybistes/core/repositories/ExchangeRepository.java
+++ backend/core/src/main/java/com/kollybistes/core/repositories/ExchangeRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TradeRepository extends JpaRepository<Exchange, Long> {
+public interface ExchangeRepository extends JpaRepository<Exchange, Long> {
 }
