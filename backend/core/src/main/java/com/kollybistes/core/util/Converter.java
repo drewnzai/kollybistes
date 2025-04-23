26b86137aef438baca47c743e9b031495d79fd64
Andrew Kombe <andrewnzaikombe@gmail.com>
2025-04-23 21:27:34 +0300

    Created a util class for unit conversions

---
+++ backend/core/src/main/java/com/kollybistes/core/util/Converter.java
@@ -0,0 +1,30 @@
+package com.kollybistes.core.util;
+
+import org.web3j.utils.Convert;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class Converter {
+
+    private Converter(){
+        // Utility class
+    }
+
+    public static BigDecimal convertWeiToEth(BigInteger wei){
+        return Convert.fromWei(wei.toString(), Convert.Unit.ETHER);
+    }
+
+    public static BigInteger convertEthToWei(BigDecimal eth){
+        return Convert.toWei(eth, Convert.Unit.ETHER).toBigInteger();
+    }
+
+    public static BigInteger convertBtcToSats(BigDecimal btc) {
+        return btc.multiply(BigDecimal.valueOf(100_000_000L)).toBigInteger();
+    }
+
+    public static BigDecimal convertSatsToBtc(BigInteger sats) {
+        return new BigDecimal(sats).divide(BigDecimal.valueOf(100_000_000L));
+    }
+
+}
