b90b76c2969abbf89d208b9d7d016d688922c808
Andrew Kombe <andrewnzaikombe@gmail.com>
2025-04-24 13:30:06 +0300

    Removed try-catch blocks

--- backend/core/src/main/java/com/kollybistes/core/api/AuthController.java
+++ backend/core/src/main/java/com/kollybistes/core/api/AuthController.java
@@ -1,15 +1,12 @@
 package com.kollybistes.core.api;
 
 
-import com.kollybistes.core.util.ErrorResponse;
 import com.kollybistes.common.dtos.LoginRequest;
 import com.kollybistes.common.dtos.RefreshTokenRequest;
 import com.kollybistes.common.dtos.RegisterRequest;
 import com.kollybistes.core.services.AuthService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -32,21 +29,8 @@ public class AuthController {
     }
 
     @PostMapping("login")
-    public Object login(@RequestBody LoginRequest loginRequest){
-
-        try{
+    public Object login(@RequestBody LoginRequest loginRequest) throws Exception {
             return authService.login(loginRequest);
-        }
-        catch(BadCredentialsException e){
-            return ErrorResponse.builder().error("Wrong password").build();
-        }
-        catch(UsernameNotFoundException | NullPointerException e){
-            return ErrorResponse.builder().error("Username does not exist").build();
-        }
-        catch(Exception e){
-            return ErrorResponse.builder().error("Verify account").build();
-        }
-
     }
 
     @PostMapping("refresh")
