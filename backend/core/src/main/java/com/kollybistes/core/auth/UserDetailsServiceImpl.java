2c6dcf9aab0bb41e436bb261d668e8ceaaee3965
Andrew Kombe <andrewnzaikombe@gmail.com>
2025-04-24 13:42:30 +0300

    Added a way to check if a given user is verified or not

--- backend/core/src/main/java/com/kollybistes/core/auth/UserDetailsServiceImpl.java
+++ backend/core/src/main/java/com/kollybistes/core/auth/UserDetailsServiceImpl.java
@@ -1,6 +1,7 @@
 package com.kollybistes.core.auth;
 
 import com.kollybistes.common.models.User;
+import com.kollybistes.core.exceptions.UserNotVerifiedException;
 import com.kollybistes.core.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,11 +20,13 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username).orElseThrow(
-                () -> {
-                    return new UsernameNotFoundException("Username not found");
-                }
+                () -> new UsernameNotFoundException("Username not found")
         );
 
+        if(!user.isEnabled()){
+            throw new UserNotVerifiedException("User is not verified. Please check email and verify");
+        }
+        
         return new UserDetailsImpl(user);
     }
 
