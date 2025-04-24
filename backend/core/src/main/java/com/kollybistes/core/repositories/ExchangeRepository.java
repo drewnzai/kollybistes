26363a942dab93c8f385cc6327e09df799956f0b
Andrew Kombe <andrewnzaikombe@gmail.com>
2025-04-24 13:07:42 +0300

    Added method to return exchanges by given user

--- backend/core/src/main/java/com/kollybistes/core/repositories/ExchangeRepository.java
+++ backend/core/src/main/java/com/kollybistes/core/repositories/ExchangeRepository.java
@@ -1,9 +1,13 @@
 package com.kollybistes.core.repositories;
 
 import com.kollybistes.common.models.Exchange;
+import com.kollybistes.common.models.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ExchangeRepository extends JpaRepository<Exchange, Long> {
+    Page<Exchange> findAllByUser(User user, Pageable pageable);
 }
