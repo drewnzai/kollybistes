e162dab42e0031a4743d4c756bc7a4c828a4bc36
Andrew Kombe <andrewnzaikombe@gmail.com>
2025-04-24 12:56:52 +0300

    Added method to return transactions by given user

--- backend/core/src/main/java/com/kollybistes/core/repositories/TransactionRepository.java
+++ backend/core/src/main/java/com/kollybistes/core/repositories/TransactionRepository.java
@@ -1,9 +1,13 @@
 package com.kollybistes.core.repositories;
 
 import com.kollybistes.common.models.Transaction;
+import com.kollybistes.common.models.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    Page<Transaction> findAllByUser(User user, Pageable pageable);
 }
