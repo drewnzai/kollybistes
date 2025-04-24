47ee51a15d71deeebad531dd8e347c8c29785dc8
Andrew Kombe <andrewnzaikombe@gmail.com>
2025-04-24 13:20:53 +0300

    Added path to get exchanges

--- backend/core/src/main/java/com/kollybistes/core/api/ExchangeController.java
+++ backend/core/src/main/java/com/kollybistes/core/api/ExchangeController.java
@@ -1,12 +1,12 @@
 package com.kollybistes.core.api;
 
 import com.kollybistes.common.dtos.ExchangeDto;
+import com.kollybistes.core.misc.PaginationRequest;
+import com.kollybistes.core.misc.PagingResult;
 import com.kollybistes.core.services.ExchangeService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/exchange/")
@@ -14,6 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExchangeController {
 
     private final ExchangeService exchangeService;
+
+    @GetMapping
+    public PagingResult<ExchangeDto> getExchanges(
+            @RequestParam(required = false) Integer page,
+            @RequestParam(required = false) Integer size,
+            @RequestParam(required = false) String sortField,
+            @RequestParam(required = false) Sort.Direction direction
+    ){
+        final PaginationRequest paginationRequest = new PaginationRequest(page, size, sortField, direction);
+
+        return exchangeService
+                .getExchanges(paginationRequest);
+    }
 
     @PostMapping("calculate")
     public Object calculateExchangeDetails(@RequestBody ExchangeDto exchangeDto) throws Exception {
