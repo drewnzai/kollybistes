18e0318791c33df1af909cdddf19762906880955
Andrew Kombe <andrewnzaikombe@gmail.com>
2025-04-28 14:10:30 +0300

    Enabled caching

--- backend/core/src/main/java/com/kollybistes/core/CoreApplication.java
+++ backend/core/src/main/java/com/kollybistes/core/CoreApplication.java
@@ -3,6 +3,7 @@ package com.kollybistes.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @ComponentScan(basePackages = {"com.kollybistes.core"})
 @EntityScan(basePackages = {"com.kollybistes.common.models"})
 @EnableJpaRepositories(basePackages = "com.kollybistes.core.repositories")
+@EnableCaching
 @EnableAsync
 public class CoreApplication {
     public static void main(String[] args) {
