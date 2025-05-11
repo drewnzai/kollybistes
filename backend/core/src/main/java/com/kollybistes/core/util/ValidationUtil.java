b6cdf5fb9214df36f4da16c133544e50595f0a18
Andrew Kombe <andrewnzaikombe@gmail.com>
2025-05-11 16:33:21 +0300

    Added annotation for no args constructor

--- backend/core/src/main/java/com/kollybistes/core/util/ValidationUtil.java
+++ backend/core/src/main/java/com/kollybistes/core/util/ValidationUtil.java
@@ -1,13 +1,13 @@
 package com.kollybistes.core.util;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ValidationUtil {
 
     private static final String ETHEREUM_ADDRESS_REGEX = "^0x[a-fA-F0-9]{40}$";
     private static final String BITCOIN_ADDRESS_REGEX = "^(bc1|bcrt1|[13])[a-zA-HJ-NP-Z0-9]{25,39}$";
-
-    private ValidationUtil() {
-        // Utility class
-    }
 
     public static boolean isValidEthereumAddress(String address) {
         return address != null && address.matches(ETHEREUM_ADDRESS_REGEX);
